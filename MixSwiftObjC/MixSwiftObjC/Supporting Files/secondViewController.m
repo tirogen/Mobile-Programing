5ea24a46890cf567e790e53784546acf9deeb6d8
Unknown <the-px@hotmail.com>
2019-03-15 16:00:07 +0700

    Mix Swift and Objective C
    also how to get request api

---
+++ MixSwiftObjC/MixSwiftObjC/Supporting Files/secondViewController.m
@@ -0,0 +1,32 @@
+//
+//  secondViewController.m
+//  MixSwiftObjC
+//
+//  Created by Ni Tu on 15/3/2562 BE.
+//  Copyright © 2562 CatDev. All rights reserved.
+//
+
+#import "secondViewController.h"
+
+@interface secondViewController ()
+
+@end
+
+@implementation secondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ MixSwiftObjC/MixSwiftObjC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MixSwiftObjC
+//
+//  Created by Ni Tu on 15/3/2562 BE.
+//  Copyright © 2562 CatDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
