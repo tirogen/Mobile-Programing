209eac14402b24953c83953608b8aea2f84eee2c
Unknown <the-px@hotmail.com>
2019-04-05 11:28:25 +0700

    new Intent

--- Android/Intent/app/src/main/java/com/e/intent/Main2Activity.java
+++ Android/Intent/app/src/main/java/com/e/intent/Main2Activity.java
@@ -1,13 +1,28 @@
 package com.e.intent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    private TextView textName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        textName = findViewById(R.id.showName);
+        textName.setText("Hello, "+name);
+    }
+
+    public void onBack(View v){
+        finish();
+        //startActivity(new Intent(this,MainActivity.class));
     }
 }
--- Android/Intent/app/src/main/java/com/e/intent/MainActivity.java
+++ Android/Intent/app/src/main/java/com/e/intent/MainActivity.java
@@ -1,13 +1,91 @@
 package com.e.intent;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editName;
+    private ImageView imageView;
+    private static final int REQUEST_TAKE_PHOTO = 1;
+    private String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editName = findViewById(R.id.editName);
+        imageView = findViewById(R.id.imageCam);
+    }
+
+    public void onClickBtn(View v){
+        String name = editName.getText().toString();
+        Intent intent = new Intent(this,Main2Activity.class);
+        intent.putExtra("name", name);
+        startActivity(intent);
+    }
+
+    public void onURL(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.eng.chula.ac.th/th/"));
+        startActivity(intent);
+    }
+
+    public void onCam(View v) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            }
+            catch (IOException ex) {
+
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this, "com.e.intent.fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName, /* prefix */
+                ".jpg", /* suffix */
+                storageDir /* directory */
+        );
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath);
+            imageView.setImageBitmap(bitmap);
+        }
     }
 }
